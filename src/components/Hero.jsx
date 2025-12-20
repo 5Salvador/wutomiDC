f47e30b8f8687091f5906fc7d9c8802f32247b28
5Salvador <salvadoramerico155@gmail.com>
2025-12-20 11:16:55 +0200

    wutomi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,8 @@ const Hero = () => {
     <>
     <section className="relative w-full h-[600px]">
         <img src={heroImage} alt="Hero" className="w-full h-full object-cover opacity-80" />
-        <div className='absolute top-1/2 left-1/2 md:left-1/5 transform -translate-x-1/2 -translate-y-1/2  flex shadow-lg'>
+        <div className='absolute top-1/2 left-1/2 md:left-1/5 transform -translate-x-1/2 -translate-y-1/2  flex flex-col shadow-lg'>
+        <span className='text-2xl md:text-6xl font-bold  text-primary'>Wutomi<span className='text-secondary'>DC</span></span>
             <p className="text-2xl md:text-5xl font-bold text-center  text-white italic">Onde a natureza gera Vida</p>
         </div>
     </section>

f6fb0f3518c3c0ea5f770b40a527f611712701fa
5Salvador <salvadoramerico155@gmail.com>
2025-12-20 11:55:21 +0200

    wutomi

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,104 @@
-import React from 'react'
-import Hero from '../../components/Hero'
+import React from "react";
+import Hero from "../../components/Hero";
+import { GiPlantWatering } from "react-icons/gi";
+import { RiUserCommunityFill } from "react-icons/ri";
+import { FaCross } from "react-icons/fa";
+import { IoFastFood } from "react-icons/io5";
+import { FaGraduationCap } from "react-icons/fa6";
+import { PiCowFill } from "react-icons/pi";
+import Picture1 from "../../assets/Picture1.jpg"
+import Picture2 from "../../assets/Picture2.jpg"
+
 
 const Home = () => {
   return (
     <>
-    <Hero />
-    </>
-  )
-}
+      <Hero />
+      {/*Mission, Vision & images*/}
+      <section className="w-ful min-h-screen">
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row  justify-center">
+          {/*First Column*/}
+          <div className="w-full md:w-1/3 flex flex-col">
+            <div className="flex flex-col justify-center bg-gray-300/40 p-4 px-8 py-4">
+              <span className="text-start font-medium text-2xl text-primary">
+                Missão
+              </span>
+              <p>
+                “Promover vida, bem-estar e desenvolvimento sustentável,
+                capacitando pessoas e comunidades através da agricultura,
+                educação, nutrição e fé.”
+              </p>
+            </div>
 
-export default Home
+            <div className="flex flex-col justify-center bg-primary/20 p-4 px-8 py-4">
+              <span className="text-start font-medium text-2xl text-primary">
+                Visão
+              </span>
+              <p>
+                “Ser referência em sustentabilidade, educação agroalimentar e
+                desenvolvimento comunitário, inspirando vidas e fortalecendo a
+                conexão com a natureza e utilizando o que Deus nos ofereceu para
+                gerar vida.”
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-center  px-8 py-4 space-y-3 bg-secondary/45 border-l-[5px] border-primary rounded-bl-3xl">
+              <span className="text-start font-medium text-2xl text-primary">
+                Valores
+              </span>
+              <div className="flex flex-col space-y-2">
+                <div className="flex flex-row items-center gap-2">
+                  <div className="p-2 bg-primary/20 rounded-full">
+                    <GiPlantWatering size={20} className="text-primary" />
+                  </div>
+                  <span>Sustentabilidade</span>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <div className="p-2 bg-primary/20 rounded-full">
+                    <RiUserCommunityFill size={20} className="text-primary" />
+                  </div>
+                  <span>Communidade</span>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <div className="p-2 bg-primary/20 rounded-full">
+                    <FaCross size={20} className="text-primary" />
+                  </div>
+                  <span>Fé e propósito</span>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <div className="p-2 bg-primary/20 rounded-full">
+                    <IoFastFood size={20} className="text-primary" />
+                  </div>
+                  <span>Nutrição e saúde</span>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <div className="p-2 bg-primary/20 rounded-full">
+                    <FaGraduationCap size={20} className="text-primary" />
+                  </div>
+                  <span>Educação e capacitação</span>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <div className="p-2 bg-primary/20 rounded-full">
+                    <PiCowFill size={20} className="text-primary" />
+                  </div>
+                  <span>Respeito à produção agrícola e animal</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/*Second Column*/}
+          <div className="w-full md:w-1/3">
+            <img src={Picture1} alt="" />
+          </div>
+          {/*Third Column*/}
+          <div className="w-full md:w-1/3">
+            <img src={Picture2} alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
